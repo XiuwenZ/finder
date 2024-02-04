4805cdbff41982a7736f51682d1eafa20b4b3462
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2024-02-04 01:35:23 -0500

    fixed Login and App

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,8 @@ const Login = () => {
 
   const handleLogin = async (event) => {
     event.preventDefault();
-
+    console.log('Handling login...');
+  
     try {
       const response = await fetch('http://localhost:3001/api/users/login', {
         method: 'POST',
@@ -21,23 +22,20 @@ const Login = () => {
         },
         body: JSON.stringify({ email, password }),
       });
-
+  
       if (response.ok) {
+        console.log('Login successful!');
         const { token } = await response.json();
-
-        // Store the token in localStorage or a state management system
         localStorage.setItem('token', token);
-
-        // Navigate to the dashboard
         navigate('/dashboard');
       } else {
-        // Handle login failure
         console.log('Login failed. Invalid email or password.');
       }
     } catch (error) {
       console.error('Error during login:', error.message);
     }
   };
+  
 
   return (
     <div className="auth-container">
