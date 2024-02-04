d088337765d64621b3bf5e383a6ce514442d07c5
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2024-02-03 22:38:16 -0500

    added footer to the page to branch nora

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import Login from './components/Login.jsx'; // Adjust the import path as necessary
 import Signup from './components/Signup.jsx'; // Adjust the import path as necessary
 import Mainpage from './components/MainPage.jsx'; // Adjust the import path as necessary
+import Footer from './components/Footer.jsx'; // Adjust the import path as necessary
+
 
 const router = createBrowserRouter([
   {
@@ -23,10 +25,9 @@ const router = createBrowserRouter([
 ]);
 
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
     <RouterProvider router={router} />
+    <Footer /> {/* Include the Footer component */}
   </React.StrictMode>,
 )
